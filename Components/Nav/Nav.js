1e6dfe14ddcd7366a0b39b169eca4a4f81d77587
Ana Paula Cortes <anapaulacortesibarra@gmail.com>
2022-06-23 13:42:49 -0300

    css

--- Components/Nav/Nav.js
+++ Components/Nav/Nav.js
@@ -1,4 +1,4 @@
-import { AiOutlineHome, AiOutlineUser } from 'react-icons/ai'
+import { AiOutlineUser } from 'react-icons/ai'
 import { FaTools } from 'react-icons/fa'
 import { MdContactMail, MdWork } from 'react-icons/md'
 import style from './Nav.module.css'
@@ -11,7 +11,7 @@ const Nav = () => {
 
     <nav className={style.Navbar}>
       <div className={style.menu}>
-        <a href="#"><AiOutlineHome className={style.icon} /></a>
+        {/* <a href="#"><AiOutlineHome className={style.icon} /></a> */}
         <a href="#about"><AiOutlineUser className={style.icon} /></a>
         <a href="#experience" ><FaTools className={style.icon} /></a>
         <a href="#contact" ><MdContactMail className={style.icon} /></a>
